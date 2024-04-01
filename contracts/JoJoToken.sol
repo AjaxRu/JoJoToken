286540140f0a7a0e711daa8cd09ed9789dbebfea
AjaxRu <145920622+AjaxRu@users.noreply.github.com>
2024-04-01 23:27:53 +0300

    Add files via upload

---
+++ contracts/JoJoToken.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract JoJo is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("JoJo", "BB") {
+        _mint(msg.sender, initialSupply);
+    }
+}
