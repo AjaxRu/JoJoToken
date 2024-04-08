1e334e02f9fec5a2017af79c26c158e5d449ac7a
AjaxRu <145920622+AjaxRu@users.noreply.github.com>
2024-04-08 17:23:46 +0300

    Update JoJoToken.sol

--- contracts/JoJoToken.sol
+++ contracts/JoJoToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract JoJo is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("JoJo", "BB") {
+    constructor(uint256 initialSupply) ERC20("JoJo", "BB") {
         _mint(msg.sender, initialSupply);
     }
 }
